88adafed41e564ac5744d529ee4c70f7089c9c6c
NicoLopez05 <167824882+NicoLopez05@users.noreply.github.com>
2024-04-22 19:46:49 -0500

    Add files via upload

---
+++ Evaluacion01 COMPLETO/PREGUNTA 2/script.js
@@ -0,0 +1,17 @@
+function calctemp(){
+   
+
+    var F =document.getElementById('temperatura');
+    let C=(F-32)*5/9;
+    alert="La temperatura calculada es"+ C;
+}
+
+document.getElementById('temperatura').innerHTML
+<script>
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+</script>
